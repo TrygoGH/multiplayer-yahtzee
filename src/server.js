b90510a067c53b82b308b2c69f4978e4b72a37de
TrygoGH <112850806+TrygoGH@users.noreply.github.com>
2025-06-09 04:00:40 +0200

    Fix(express): Added env port for render
    Using render env port hopefully

--- src/server.js
+++ src/server.js
@@ -43,7 +43,7 @@ process.on('unhandledRejection', (reason, promise) => {
 console.log(`Starting server at ${Date.now().toLocaleString()}`);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
